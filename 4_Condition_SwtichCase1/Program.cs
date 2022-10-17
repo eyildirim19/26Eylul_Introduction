b3cba94bc751f69b48077ddc17dbc5a9b8fb5f6f
BTAkademimcsd <53770975+BTAkademimcsd@users.noreply.github.com>
2022-10-17 21:37:21 +0300

    döngüler_for

--- 4_Condition_SwtichCase1/Program.cs
+++ 4_Condition_SwtichCase1/Program.cs
@@ -84,6 +84,105 @@
 
             Console.WriteLine("Test");
 
+
+
+            string ay = "ŞUbAt";
+            string mevsim = "";
+            int aaa;
+            switch (ay.ToUpper())
+            {
+                case "OCAK" or "ŞUBAT" or "MART":
+                    mevsim = "KIŞ";
+
+                    if (ay == "OCAK")
+                        aaa = 1;
+                    else if (ay == "ŞUBAT")
+                        aaa = 2;
+                    else
+                        aaa = 3;
+
+                    break;
+                case "HAZİRAN":
+                case "NİSAN":
+                case "MAYIS":
+                    mevsim = "İLK BAHAR";
+                    aaa = 4; // geçici değer atadım. bunu silersiniz...
+                    break;
+                default:
+                    mevsim = "BULUNAMADI";
+                    aaa = -1;
+                    break;
+            }
+
+            Console.WriteLine($"MEVSİMLERDEN {mevsim}. AYLARDAN {aaa}. AY");
+
+
+            if (ay.ToUpper() == "OCAK" || ay.ToUpper() == "ŞUBAT" || ay.ToUpper() == "MART")
+            {
+                //Console.WriteLine("KIŞ");
+                mevsim = "KIŞ";
+            }
+            else if (ay.ToUpper() == "NİSAN" || ay.ToUpper() == "MAYIS" || ay.ToUpper() == "HAZİRAN")
+            {
+                //Console.WriteLine("İLK BAHAR");
+                mevsim = "İLK BAHAR";
+            }
+            else
+            {
+                mevsim = "BULUNAMADI";
+            }
+            Console.WriteLine($"MEVSİM {mevsim}");
+
+
+
+
+
+            float vize = 50, final = 30, ort;
+
+            //vize = Convert.ToSingle(Console.ReadLine());
+            //final = Convert.ToSingle(Console.ReadLine());
+
+            ort = vize * 0.4f + final * 0.6f;
+
+
+            string harfNot = "";
+
+            if (ort > 85 && ort < 100)
+            {
+                harfNo = "AA";
+            }
+
+            
+            // when yapısı
+            switch (ort)
+            {
+                case float f when f >= 85 && f <= 100:
+                    harfNo = "AA";
+                    break;
+                case float f when f >= 70 && f <= 84:
+                    harfNo = "BB";
+                    break;
+                default:
+                    break;
+            }
+            // veya...
+
+            // relational yapısı
+            switch (ort)
+            {
+                case > 85 and < 100:
+                    harfNo = "AA";
+                    break;
+                case > 70 and < 84:
+                    harfNo = "AA";
+                    break;
+            }
+            // veya
+            
+            Console.WriteLine($"ortalamınız {ort} harf notunuz  {harfNot}");
+
+
+
             Console.WriteLine("Hello, World!");
         }
     }
---
+++ 5_Loops/Program.cs
@@ -0,0 +1,85 @@
+﻿namespace _5_Loops
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Döngü Nedir");
+
+
+
+            Console.WriteLine("Merhaba Dünya");
+            Console.WriteLine("Merhaba Dünya");
+            Console.WriteLine("Merhaba Dünya");
+            Console.WriteLine("Merhaba Dünya");
+            Console.WriteLine("Merhaba Dünya");
+            Console.WriteLine("Merhaba Dünya");
+            Console.WriteLine("Merhaba Dünya");
+            Console.WriteLine("Merhaba Dünya");
+            Console.WriteLine("Merhaba Dünya");
+            Console.WriteLine("********************");
+            /*
+                    
+             kodların belirli bir koşul doğrultusan tekrar çalışmasını sağlayan program yapılarıdır..
+
+            döngüler;
+            1)for
+            2)while
+            3)do while
+
+            dizilerden sonra işlenecek;
+            4)foreach
+             */
+
+            /*
+                for döngüsü => koşul doğrultusunda gövdesindeki kodları tekrar çalıştaran bloktur
+            for(<baslangic_deger>; <kosul>;<baslangicdeger_manipulasyon>)
+            {
+                <koşullu kod>
+            } 
+
+             1 önce başlangıç değer (bir kere çalışır);
+             2 koşul
+             3 koşullu kod
+             4 artım veya azalım
+            // sonraki süreçler 
+            4 2 3
+            */
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("Merhaba Dünya");
+            }
+            Console.WriteLine("********************");
+            for (int i = 10; i > 0; i--)
+            {
+                Console.WriteLine("Merhaba Dünya");
+            }
+            Console.WriteLine("********************");
+            for (int i = 10; i < 100; i += 10)
+            {
+                Console.WriteLine("Merhaba Dünya");
+            }
+            Console.WriteLine("********************");
+            // i' kod birkere çalıştıktan sonra z  olduğu için  mesaj bir kere gösterilir...
+            for (string i = "a"; i != "z"; i = "z")
+            {
+                Console.WriteLine("Merhaba Dünya");
+            }
+
+            // 
+            for (int i = 0; i < 100; i++)
+            {
+                Console.Write($"{i + 1} ");
+            }
+
+            // 2022 2021 2020 ............2000
+            for (int i = 2022; i >= 2000; i--)
+                Console.Write($"{i}");
+            Console.WriteLine("***************");
+            for (int i = 2022; i >= 2000;)
+                Console.Write($"{i--}"); // 2022 ......2000
+
+            Console.ReadKey();
+        }
+    }
+}
