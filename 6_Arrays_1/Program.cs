fe3bb3f79576e7ee03f471f0d66d2cb246f9e09d
BTAkademimcsd <53770975+BTAkademimcsd@users.noreply.github.com>
2022-10-24 21:46:44 +0300

    diziler

---
+++ 6_Arrays_1/Program.cs
@@ -0,0 +1,73 @@
+﻿namespace _6_Arrays_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string gun = "Pazartesi";
+            string[] gunler = new string[7];
+            //gunler = gun;
+            gunler[0] = gun;
+            gunler[1] = "Salı";
+            gunler[2] = "Çarşamba";
+            gunler[3] = "Perşembe";
+            gunler[4] = "Cuma";
+            gunler[5] = "Cumartesi";
+            gunler[6] = "Pazar";
+
+            Console.WriteLine($"dizinin eleman sayısı {gunler.Length}");
+            int j = gunler.Length - 1;
+            while (j > -1)
+                Console.WriteLine(gunler[j--]); // 
+
+            for (int i = 0; i < gunler.Length; i++)
+                Console.WriteLine(gunler[i]);
+
+            foreach (string gn in gunler)
+                Console.WriteLine(gn);
+
+            // s değişkeni dizinin içerisinden okunan elemanı tutar.in ifadesi dizin içerisinden sıradaki elemanı alır.. ve değişkenene aktarır.
+            // foreach döngüsü ileri yönlü okuma yapar
+            //foreach (string s in gunler)
+            //{
+            //    string temp = s; // s'nin değerini oku
+            //    //s = "deneme"; // s'ye değer ata... // buradaki s değişkenine atama işlemi yapılamaz..
+            //}
+            // dizi tanımla 2
+            string[] guns = { "Pazartesi", "Salı", "Çarşamba", "Perşembe", "Cuma" };
+            int a = 4, b = 2, c = 8, d = 10;
+            int[] nums = { a, b, c, d };
+
+            int enb = nums[0]; // dizinin 0. elemanı en büyük kabul edilir
+            for (int i = 1; i < nums.Length; i++)
+                if (nums[i] > enb)
+                    enb = nums[i];
+
+            enb = nums[0];
+            foreach (int n in nums)
+                if (n > enb)
+                    enb = n;
+
+            Console.WriteLine($"en büyük eleman {enb}");
+
+            Console.WriteLine("**************************************");
+
+            char[] chars = { 'a', 'b', 'a', 'a', 'd', 'e', 'e', 'c' };
+            char[] charunique = new char[0];
+
+            for (int i = 0; i < chars.Length; i++)
+            {
+                int adet = 0;
+                for (int k = 0; k < chars.Length; k++)
+                {
+                    if (chars[i] == chars[k])
+                        adet++;
+                }
+                Console.WriteLine($"{chars[i]} elemanından {adet} var");
+            }
+
+
+
+        }
+    }
+}
---
+++ 6_Arrays_3/Program.cs
@@ -0,0 +1,48 @@
+﻿namespace _6_Arrays_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Array sınıfı diziler üzerinde işlem yapmamızı sağlayan bir sınıftır. bu sınıfın metotları ile diziler üzerinde çeşitli işlemler yapabiliriz. 
+            // Resize
+
+            // new => bellekte yeni bir alan açar...
+            int[] nums = new int[2]; // açılan  alanı nums değişkeninde tutulur
+            nums[0] = 1;
+            nums[1] = 2;
+
+            //nums = new int[3]; // açılan alan nums değişkeninde tutulur..
+            // new ifadesini kullandığımız için önceki elemanlar kaybolur 3 elemanlı yeni bir alan açılır.
+            Array.Resize(ref nums, 3); // Resize metodu dizinin uzunluğunu değiştirmek için kullanılır. 2 parametre alır. ilk parametre uzunluğu değişik dizi, 2. paramre yeni uzunluk...
+            nums[2] = 10;
+            foreach (int i in nums)
+                Console.Write(i); // 1 2 10
+
+            Console.WriteLine("****");
+            Array.Resize(ref nums, 1);// yeni uzunluk 1
+
+            foreach (int j in nums)
+                Console.Write(j);
+
+            string[] kisiler = { "İsmail", "Gökhan", "Ekrem", "Cemre" };
+
+            string isim = "Gökhan";
+            foreach (string s in kisiler)
+                if (s == isim)
+                    Console.WriteLine("Dizide var..");
+
+            for (int i = 0; i < kisiler.Length; i++)
+                if (isim == kisiler[i])
+                    Console.WriteLine($"Dizide var. İndex numarası {i}");
+
+            // aranan elemanın indexini döner...eğer aranan eleman dizide varsa index'ini yoksa -1  döner. -1 dönmesinin nedeni -1 indexi yoktur..
+            int index1 = Array.IndexOf(kisiler, isim);
+            int index2 = Array.IndexOf(kisiler, "Cemre");
+            int index3 = Array.IndexOf(kisiler, "Berat");
+            int index4 = Array.IndexOf(kisiler, "İsmail");
+
+            Console.WriteLine($"sonuc= {index1} {index2} {index3} {index4}"); // 1 3 -1 0
+        }
+    }
+}
